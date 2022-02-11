76574e9fd37ed3435d2add28e623d2bfef0f20ce
Volker Scheuber <volker.scheuber@forgerock.com>
2022-02-10 22:48:08 -0600

    New binary release pipeline

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --experimental-json-modules --no-warnings --enable-source-maps
 
 import { Command } from 'commander';
 import { initConnections } from './api/AuthApi.js';
@@ -7,9 +7,10 @@ import * as idm from './commands/idm/cmd.js';
 import * as info from './commands/info/cmd.js';
 import * as journey from './commands/journey/cmd.js';
 import * as logging from './commands/logging/cmd.js';
-import fs from 'fs';
+import pkg from '../package.json';
 
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+// import fs from 'fs';
+// const pkg = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
 const program = new Command(pkg.name)
     .version(`v${pkg.version} [${process.version}]`, '-v, --version');
