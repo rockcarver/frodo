eb756719b19014520f4ee823d7cecd8e0b466fc0
Sandeep Chaturvedi <schaturvedi@gmail.com>
2022-02-15 15:16:39 -0700

    Implemented independent script export and import

--- src/api/ScriptApi.js
+++ src/api/ScriptApi.js
@@ -4,6 +4,8 @@ import util from 'util';
 import storage from '../storage/SessionStorage.js';
 
 const scriptURLTemplate = "%s/json%s/scripts/%s";
+const scriptListURLTemplate = "%s/json%s/scripts?_queryFilter=true";
+const scriptQueryURLTemplate = "%s/json%s/scripts?_queryFilter=name+eq+%%22%s%%22";
 const apiVersion = 'protocol=2.0,resource=1.0';
 const getApiConfig = () => {
     const configPath = getCurrentRealmPath();
@@ -12,6 +14,42 @@ const getApiConfig = () => {
         apiVersion,
     };
 };
+
+export async function listScripts() {
+    try {
+        const urlString = util.format(scriptListURLTemplate, storage.session.getTenant(), getCurrentRealmPath());
+        const response = await generateAmApi(getApiConfig()).get(
+            urlString,
+            { withCredentials: true }
+        );
+        if (response.status < 200 || response.status > 399) {
+            console.error("listScripts ERROR: list scripts structure call returned %d, possible cause: scripts not found", response.status);
+            return null;
+        }
+        return response.data.result;
+    } catch (e) {
+        console.error("listScripts ERROR: list script structure error - ", e);
+        return null;
+    }
+}
+
+export async function getScriptByName(name) {
+    try {
+        const urlString = util.format(scriptQueryURLTemplate, storage.session.getTenant(), getCurrentRealmPath(), name);
+        const response = await generateAmApi(getApiConfig()).get(
+            urlString,
+            { withCredentials: true }
+        );
+        if (response.status < 200 || response.status > 399) {
+            console.error("getScriptByName ERROR: get script structure call returned %d, possible cause: script not found", response.status);
+            return null;
+        }
+        return response.data.result;
+    } catch (e) {
+        console.error("getScriptByName ERROR: get script structure error - ", e.message);
+        return null;
+    }
+}
 
 export async function getScript(id) {
     try {
