8996eac2ab616d5410f0ba71425390974cc9b348
Volker Scheuber <volker.scheuber@forgerock.com>
2022-05-30 13:32:57 -0500

    Template structured command files

---
+++ src/cli/_template/command-sub.js
@@ -0,0 +1,39 @@
+import { Command, Option } from 'commander';
+import * as common from '../cmd_common.js';
+import { getTokens } from '../../api/AuthApi.js';
+import storage from '../../storage/SessionStorage.js';
+import { printMessage } from '../../api/utils/Console.js';
+import { listProviders } from '../../ops/SamlOps.js';
+
+const program = new Command('frodo command sub');
+
+program
+  .description('Sub-command.')
+  .helpOption('-h, --help', 'Help')
+  .showHelpAfterError()
+  .addArgument(common.hostArgumentM)
+  .addArgument(common.realmArgument)
+  .addArgument(common.userArgument)
+  .addArgument(common.passwordArgument)
+  .addOption(common.deploymentOption)
+  .addOption(common.insecureOption)
+  .addOption(
+    new Option('-l, --long', 'Long with all fields.').default(false, 'false')
+  )
+  .action(
+    // implement command logic inside action handler
+    async (host, realm, user, password, options) => {
+      storage.session.setTenant(host);
+      storage.session.setRealm(realm);
+      storage.session.setUsername(user);
+      storage.session.setPassword(password);
+      storage.session.setDeploymentType(options.type);
+      storage.session.setAllowInsecureConnection(options.insecure);
+      if (await getTokens()) {
+        // code goes here
+      }
+    }
+    // end command logic inside action handler
+  );
+
+program.parse();
