7e4f236ad8bae653784bafed8c9b6957a2310431
Volker Scheuber <volker.scheuber@forgerock.com>
2022-06-18 19:09:06 -0700

    Added journey delete command

---
+++ src/cli/journey/journey-delete.js
@@ -0,0 +1,67 @@
+import { Command, Option } from 'commander';
+import * as common from '../cmd_common.js';
+import { getTokens } from '../../api/AuthApi.js';
+import { deleteJourney, prune } from '../../ops/JourneyOps.js';
+import storage from '../../storage/SessionStorage.js';
+import { printMessage } from '../../ops/utils/Console.js';
+
+const program = new Command('frodo journey delete');
+
+program
+  .description('Delete journeys/trees.')
+  .helpOption('-h, --help', 'Help')
+  .showHelpAfterError()
+  .addArgument(common.hostArgumentM)
+  .addArgument(common.realmArgument)
+  .addArgument(common.userArgument)
+  .addArgument(common.passwordArgument)
+  .addOption(common.deploymentOption)
+  .addOption(common.insecureOption)
+  .addOption(
+    new Option(
+      '-i, --journey-id <journey>',
+      'Name of a journey/tree. If specified, -a is ignored.'
+    )
+  )
+  .addOption(
+    new Option(
+      '--no-deep',
+      'No deep delete. This leaves orphaned configuration artifacts behind.'
+    )
+  )
+  .addOption(
+    new Option(
+      '--verbose',
+      'Verbose output during command execution. If specified, may or may not produce additional output.'
+    ).default(false, 'off')
+  )
+  .action(
+    // implement command logic inside action handler
+    async (host, realm, user, password, options) => {
+      storage.session.setTenant(host);
+      storage.session.setRealm(realm);
+      storage.session.setUsername(user);
+      storage.session.setPassword(password);
+      storage.session.setDeploymentType(options.type);
+      storage.session.setAllowInsecureConnection(options.insecure);
+      if (await getTokens()) {
+        // delete by id
+        if (options.journeyId) {
+          printMessage(
+            `Deleting journey ${
+              options.journeyId
+            } in realm "${storage.session.getRealm()}"...`
+          );
+          deleteJourney(options.journeyId, options);
+        }
+        // unrecognized combination of options or no options
+        else {
+          printMessage('Unrecognized combination of options or no options...');
+          program.help();
+        }
+      }
+    }
+    // end command logic inside action handler
+  );
+
+program.parse();
