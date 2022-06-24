f447bf08959b2aaec80ed1e05a323aeb66eceb2a
Volker Scheuber <volker.scheuber@forgerock.com>
2022-06-23 20:44:44 -0500

    Restructured theme commands and added theme delete command

---
+++ src/cli/theme/theme.js
@@ -0,0 +1,23 @@
+import { Command } from 'commander';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default function setup() {
+  const program = new Command('theme')
+    .helpOption('-h, --help', 'Help')
+    .description('Manages themes.')
+    .executableDir(__dirname);
+
+  program.command('list', 'List themes.').showHelpAfterError();
+
+  program.command('export', 'Export themes.').showHelpAfterError();
+
+  program.command('import', 'Import themes.').showHelpAfterError();
+
+  program.command('delete', 'Delete themes.').showHelpAfterError();
+
+  program.showHelpAfterError();
+  return program;
+}
