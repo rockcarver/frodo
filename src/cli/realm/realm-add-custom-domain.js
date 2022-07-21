d3d816eb839b72dcbcb9ab4ed6df42a47f91cb14
Volker Scheuber <volker.scheuber@forgerock.com>
2022-07-20 22:38:59 -0500

    resolves #167 - split realm api from ops layer

--- src/cli/realm/realm-add-custom-domain.js
+++ src/cli/realm/realm-add-custom-domain.js
@@ -3,7 +3,7 @@ import * as common from '../cmd_common.js';
 import { getTokens } from '../../ops/AuthenticateOps.js';
 import storage from '../../storage/SessionStorage.js';
 import { createKeyValueTable, printMessage } from '../../ops/utils/Console.js';
-import { addCustomDomain } from '../../api/RealmApi.js';
+import { addCustomDomain } from '../../ops/RealmOps.js';
 
 const program = new Command('frodo realm add-custom-domain');
 
@@ -38,27 +38,7 @@ program
             options.domain
           } to realm ${storage.session.getRealm()}...`
         );
-        const realmConfig = await addCustomDomain(
-          storage.session.getRealm(),
-          options.domain
-        );
-        if (realmConfig != null) {
-          const table = createKeyValueTable();
-          table.push(['Name'.brightCyan, realmConfig.name]);
-          table.push([
-            'Status'.brightCyan,
-            realmConfig.active ? 'active'.brightGreen : 'inactive'.brightRed,
-          ]);
-          table.push([
-            'Custom Domains'.brightCyan,
-            realmConfig.aliases.join('\n'),
-          ]);
-          table.push(['Parent'.brightCyan, realmConfig.parentPath]);
-          table.push(['Id'.brightCyan, realmConfig._id]);
-          printMessage(table.toString());
-        } else {
-          printMessage(`No realm found with name ${options.target}`, 'warn');
-        }
+        await addCustomDomain(storage.session.getRealm(), options.domain);
       }
     }
     // end command logic inside action handler
--- src/cli/realm/realm-list.js
+++ src/cli/realm/realm-list.js
@@ -2,8 +2,8 @@ import { Command, Option } from 'commander';
 import * as common from '../cmd_common.js';
 import { getTokens } from '../../ops/AuthenticateOps.js';
 import storage from '../../storage/SessionStorage.js';
-import { createTable, printMessage } from '../../ops/utils/Console.js';
-import { listRealms } from '../../api/RealmApi.js';
+import { printMessage } from '../../ops/utils/Console.js';
+import { listRealms } from '../../ops/RealmOps.js';
 
 const program = new Command('frodo realm list');
 
@@ -31,28 +31,7 @@ program
       storage.session.setAllowInsecureConnection(options.insecure);
       if (await getTokens()) {
         printMessage('Listing all realms...');
-        const realms = await listRealms();
-        if (options.long) {
-          const table = createTable([
-            'Name'.brightCyan,
-            'Status'.brightCyan,
-            'Custom Domains'.brightCyan,
-            'Parent'.brightCyan,
-          ]);
-          realms.forEach((realmConfig) => {
-            table.push([
-              realmConfig.name,
-              realmConfig.active ? 'active'.brightGreen : 'inactive'.brightRed,
-              realmConfig.aliases.join('\n'),
-              realmConfig.parentPath,
-            ]);
-          });
-          printMessage(table.toString());
-        } else {
-          realms.forEach((realmConfig) => {
-            printMessage(realmConfig.name, 'info');
-          });
-        }
+        await listRealms(options.long);
       }
     }
     // end command logic inside action handler
